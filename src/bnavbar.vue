0abc7f3fe22e843c3b18e096fe6a8180fa519d07
EnricoCirilli <enrico.cirilli@gmail.com>
2023-11-23 14:47:00 +0100

    add: first commit

---
+++ src/bnavbar.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  data() {
+    return {
+    
+    }
+  },
+}
+
+</script>
+
+
+<template>
+<img src="" alt="">
+<h2>Logo</h2>
+<ul>
+    <li>CHARACTERS</li>    
+    <li>COMICS</li>
+    <li>MOVIE</li>
+    <li>TV</li>
+    <li>GAMES</li>
+    <li>COLLECTIBLES</li>
+    <li>VIDEOS</li>
+    <li>FANS</li>
+    <li>NEWS</li>
+    <li>SHOP</li>
+</ul>
+
+
+</template>
+
+
+<style>
+
+
+
+</style>
